0d9fcdfa98de1d464937967cc4ec11482492122a
Atul <atulanand94@gmail.com>
2015-03-17 14:19:33 +0530

    Broke the table methods into smaller chunks

--- table/table_tbl.js
+++ table/table_tbl.js
@@ -1,6 +1,4 @@
-  var tableModule = {};
-
-  tableModule.createTable = function(rows, cols) {
+  var tableModule = function() {
 
       //===========================================
       //      Dummy Data
@@ -11,221 +9,215 @@
       var columnHeaderElementData = 'colHead';
       //=============================================
       console.time('hello');
-      var tableWrap = document.createElement('div');
-      tableWrap.setAttribute('class', 'tableWrap');
-
-      var leftHeader = document.createElement('div');
-      leftHeader.setAttribute('class', 'leftHeader');
-      tableWrap.appendChild(leftHeader);
-
-      var commonHeader = document.createElement('div');
-      commonHeader.setAttribute('class', 'commonHeader cellDimensions rowHeaderWidth');
-      commonHeader.appendChild(document.createTextNode(tablename))
-      leftHeader.appendChild(commonHeader);
 
 
-      var leftRowHeader = document.createElement('div');
-      leftRowHeader.setAttribute('class', 'leftRowHeader rowHeaderWidth tableHeight');
-      leftHeader.appendChild(leftRowHeader);
-
-      var leftRowHeaderTable = document.createElement('table');
-      leftRowHeader.appendChild(leftRowHeaderTable);
-
-      var leftRowHeaderTableFrag = document.createDocumentFragment();
-
-      for (i = 0; i < rows; ++i) {
-          var rowHeaderElement = document.createElement('tr');
-          rowHeaderElement.setAttribute('class', 'rowHeaderElement cellDimensions');
-          rowHeaderElement.appendChild(document.createTextNode(rowHeaderElementData));
-          leftRowHeaderTableFrag.appendChild(rowHeaderElement);
-      }
-      leftRowHeaderTable.appendChild(leftRowHeaderTableFrag);
+      var createTable = function(rows, cols) {
 
 
+          var tableWrap = document.createElement('div');
+          tableWrap.setAttribute('class', 'tableWrap');
 
-      var tableAndColHeader = document.createElement('div');
-      tableAndColHeader.setAttribute('class', 'tableAndColHeader');
-      tableWrap.appendChild(tableAndColHeader);
+          var leftHeader = _createTableElement('div', 'leftHeader', tableWrap);
+          var commonHeader = _createTableElement('div', 'commonHeader cellDimensions rowHeaderWidth', leftHeader);
+          _insertText(commonHeader, tablename);
 
-      var tableColHeader = document.createElement('div');
-      tableColHeader.setAttribute('class', 'tableColHeader tableWidth');
-      tableAndColHeader.appendChild(tableColHeader);
+          var leftRowHeader = _createLeftRowHeader(rows, leftHeader);
+          var tableAndColHeader = _createTableElement('div', 'tableAndColHeader', tableWrap);
+          var tableColHeader = _createColHeader(cols, tableAndColHeader);
 
-      var tableColHeaderTable = document.createElement('table');
-      tableColHeader.appendChild(tableColHeaderTable);
 
-      var tableColHeaderTableFrag = document.createDocumentFragment();
+          var tableBody = _createTableBody(rows, cols, tableAndColHeader);
 
-      for (i = 0; i < cols; ++i) {
-          var colHeaderElement = document.createElement('th');
-          colHeaderElement.setAttribute('class', 'colHeaderElement cellDimensions');
-          colHeaderElement.appendChild(document.createTextNode(columnHeaderElementData));
-          tableColHeaderTableFrag.appendChild(colHeaderElement);
+          //fixes the width calculation and also makes sure both row header and table are side to side
+          _fixTablebodyWidth(tableBody, leftHeader, tableAndColHeader);
+
+
+          //adding event listeners
+          tableBody.addEventListener('scroll', function() {
+              tableColHeader.scrollLeft = tableBody.scrollLeft;
+              leftRowHeader.scrollTop = tableBody.scrollTop;
+          });
+          tableColHeader.addEventListener('scroll', function() {
+              tableBody.scrollLeft = tableColHeader.scrollLeft;
+          });
+
+          leftRowHeader.addEventListener('scroll', function() {
+              tableBody.scrollTop = leftRowHeader.scrollTop;
+          });
+
+          console.timeEnd('hello');
+          return tableWrap;
+      };
+
+
+      var _fixTablebodyWidth = function(tableBody, leftHeader, tableAndColHeader) {
+          //fixes the width calculation and also makes sure both row header and table are side to side
+          var tableBodyWidth = window.getComputedStyle(tableBody).getPropertyValue('width');
+          var leftHeaderWidth = window.getComputedStyle(leftHeader).getPropertyValue('width');
+          tableAndColHeader.style.width = parseInt(tableBodyWidth) + parseInt(leftHeaderWidth) + 5 + 'px';
+
       }
 
-      tableColHeaderTable.appendChild(tableColHeaderTableFrag);
 
-      var tableBody = document.createElement('div');
-      tableBody.setAttribute('class', 'tableBody tableWidth tableHeight');
-      tableAndColHeader.appendChild(tableBody);
-
-      var tableBodyTable = document.createElement('table');
-      tableBody.appendChild(tableBodyTable);
-
-      var tableBodyTableFrag = document.createDocumentFragment();
-
-      for (i = 0; i < rows; ++i) {
-          var tableRow = document.createElement('tr');
-          tableRow.setAttribute('class', 'tableRow');
-          tableBodyTableFrag.appendChild(tableRow);
-
-          var tableRowFrag = document.createDocumentFragment();
-
-          for (j = 0; j < cols; ++j) {
-              var cell = document.createElement('td');
-              cell.setAttribute('class', 'cell cellDimensions');
-              cell.appendChild(document.createTextNode(celldata));
-              tableRowFrag.appendChild(cell);
+      var _createTableBody = function(rows, columns, parentNode) {
+          var tableBodydiv = _createTableElement('div', 'tableBody tableWidth tableHeight', parentNode);
+          var tableBodyTable = _createTableElement('table', '', tableBodydiv);
+          for (i = 0; i < rows; ++i) {
+              var tableRow = _createTableElement('tr', 'tableRow', tableBodyTable);
+              for (j = 0; j < columns; ++j) {
+                  var cell = _createTableElement('td', 'cell cellDimensions', tableRow);
+                  _insertText(cell, celldata);
+              }
           }
-          tableRow.appendChild(tableRowFrag);
+          return tableBodydiv;
       }
 
-      tableBodyTable.appendChild(tableBodyTableFrag);
 
+      var _createColHeader = function(columns, parentNode) {
+          var tableColHeaderdiv = _createTableElement('div', 'tableColHeader tableWidth', parentNode);
+          var tableColHeaderTable = _createTableElement('table', '', tableColHeaderdiv);
+          for (i = 0; i < columns; ++i) {
+              var colHeaderElement = _createTableElement('th', 'colHeaderElement cellDimensions', tableColHeaderTable);
+              _insertText(colHeaderElement, columnHeaderElementData);
+          }
+          return tableColHeaderdiv;
+      }
 
-      //fixes the width calculation and also makes sure both row header and table are side to side
-      var tableBodyWidth = window.getComputedStyle(tableBody).getPropertyValue('width');
-      var leftRowHeaderWidth = window.getComputedStyle(leftRowHeader).getPropertyValue('width');
-      tableAndColHeader.style.width = parseInt(tableBodyWidth) + parseInt(leftRowHeaderWidth) + 5 + 'px';
-
-
-      //adding event listeners
-
-      tableBody.addEventListener('scroll', function() {
-          tableColHeader.scrollLeft = tableBody.scrollLeft;
-          leftRowHeader.scrollTop = tableBody.scrollTop;
-      });
-      tableColHeader.addEventListener('scroll', function() {
-          tableBody.scrollLeft = tableColHeader.scrollLeft;
-      });
-
-      leftRowHeader.addEventListener('scroll', function() {
-          tableBody.scrollTop = leftRowHeader.scrollTop;
-      });
-
-      console.timeEnd('hello');
-      return tableWrap;
-  }
+      var _createLeftRowHeader = function(rowCount, parentNode) {
+          var leftRowHeader = _createTableElement('div', 'leftRowHeader rowHeaderWidth tableHeight', parentNode);
+          var leftRowHeaderTable = _createTableElement('table', '', leftRowHeader);
+          for (i = 0; i < rowCount; ++i) {
+              var rowHeaderElement = _createTableElement('tr', 'rowHeaderElement cellDimensions', leftRowHeaderTable);
+              _insertText(rowHeaderElement, rowHeaderElementData);
+          }
+          return leftRowHeader;
+      }
 
 
 
-  tableModule.insertTable = function(insertionDivId, table) {
-      var tableInsertionDiv = document.getElementById(insertionDivId);
-      tableInsertionDiv.appendChild(table);
-  }
+      var _createTableElement = function(nodeType, classname, parentNode) {
+          var element = document.createElement(nodeType);
+          element.setAttribute('class', classname);
+          parentNode.appendChild(element);
+          return element;
+      }
+
+      var _insertText = function(node, text) {
+          node.appendChild(document.createTextNode(text));
+      }
+
+      var insertTable = function(insertionDivId, table) {
+          var tableInsertionDiv = document.getElementById(insertionDivId);
+          tableInsertionDiv.appendChild(table);
+      }
 
 
-  tableModule.insertData = function(rowHeaderData, colHeaderData, cellData, table) {
+      var insertData = function(rowHeaderData, colHeaderData, cellData, table) {
 
+      }
 
-  }
+      return {
+          createTable: createTable,
+          insertTable: insertTable,
+          insertData: insertData
+      }
+  }();
 
+  // <!-- TABLE BELOW IS GENERATED DYNAMICALLY , THIS IS LEFT FOR REFERENCE -->
+  // <div class='tableWrap'>
+  //     <div class='leftHeader'>
+  //         <div class='commonHeader cellDimensions rowHeaderWidth'>
+  //             sample
+  //         </div>
+  //         <div class='leftRowHeader rowHeaderWidth tableHeight'>
+  //             <table>
+  //                 <tr class='rowHeaderElement cellDimensions'>
+  //                     <th>RowHead</th>
+  //                 </tr>
+  //                 <tr class='rowHeaderElement cellDimensions'>
+  //                     <th>RowHead</th>
+  //                 </tr>
+  //                 <tr class='rowHeaderElement cellDimensions'>
+  //                     <th>RowHead</th>
+  //                 </tr>
+  //                 <tr class='rowHeaderElement cellDimensions'>
+  //                     <th>RowHead</th>
+  //                 </tr>
+  //                 <tr class='rowHeaderElement cellDimensions'>
+  //                     <th>RowHead</th>
+  //                 </tr>
+  //                 <tr class='rowHeaderElement cellDimensions'>
+  //                     <th>RowHead</th>
+  //                 </tr>
+  //                 <tr class='rowHeaderElement cellDimensions'>
+  //                     <th>RowHead</th>
+  //                 </tr>
+  //             </table>
+  //         </div>
+  //     </div>
+  //     <div class='tableAndColHeader'>
+  //         <div class='tableColHeader tableWidth'>
+  //             <table>
+  //                 <th class='colHeaderElement cellDimensions'> ColHeader </th>
+  //                 <th class='colHeaderElement cellDimensions'> ColHeader </th>
+  //                 <th class='colHeaderElement cellDimensions'> ColHeader </th>
+  //                 <th class='colHeaderElement cellDimensions'> ColHeader </th>
+  //                 <th class='colHeaderElement cellDimensions'> ColHeader </th>
+  //             </table>
+  //         </div>
+  //         <div class='tableBody tableWidth tableHeight'>
+  //             <table>
+  //                 <tr class='tableRow'>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                 </tr>
+  //                 <tr class='tableRow'>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                 </tr>
+  //                 <tr class='tableRow'>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                 </tr>
+  //                 <tr class='tableRow'>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                 </tr>
+  //                 <tr class='tableRow'>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                 </tr>
+  //                 <tr class='tableRow'>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                 </tr>
+  //                 <tr class='tableRow'>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                     <td class='cell cellDimensions'>cell</td>
+  //                 </tr>
+  //             </table>
+  //         </div>
+  //     </div>
 
-// <!-- TABLE BELOW IS GENERATED DYNAMICALLY , THIS IS LEFT FOR REFERENCE -->
-// <div class='tableWrap'>
-//     <div class='leftHeader'>
-//         <div class='commonHeader cellDimensions rowHeaderWidth'>
-//             sample
-//         </div>
-//         <div class='leftRowHeader rowHeaderWidth tableHeight'>
-//             <table>
-//                 <tr class='rowHeaderElement cellDimensions'>
-//                     <th>RowHead</th>
-//                 </tr>
-//                 <tr class='rowHeaderElement cellDimensions'>
-//                     <th>RowHead</th>
-//                 </tr>
-//                 <tr class='rowHeaderElement cellDimensions'>
-//                     <th>RowHead</th>
-//                 </tr>
-//                 <tr class='rowHeaderElement cellDimensions'>
-//                     <th>RowHead</th>
-//                 </tr>
-//                 <tr class='rowHeaderElement cellDimensions'>
-//                     <th>RowHead</th>
-//                 </tr>
-//                 <tr class='rowHeaderElement cellDimensions'>
-//                     <th>RowHead</th>
-//                 </tr>
-//                 <tr class='rowHeaderElement cellDimensions'>
-//                     <th>RowHead</th>
-//                 </tr>
-//             </table>
-//         </div>
-//     </div>
-//     <div class='tableAndColHeader'>
-//         <div class='tableColHeader tableWidth'>
-//             <table>
-//                 <th class='colHeaderElement cellDimensions'> ColHeader </th>
-//                 <th class='colHeaderElement cellDimensions'> ColHeader </th>
-//                 <th class='colHeaderElement cellDimensions'> ColHeader </th>
-//                 <th class='colHeaderElement cellDimensions'> ColHeader </th>
-//                 <th class='colHeaderElement cellDimensions'> ColHeader </th>
-//             </table>
-//         </div>
-//         <div class='tableBody tableWidth tableHeight'>
-//             <table>
-//                 <tr class='tableRow'>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                 </tr>
-//                 <tr class='tableRow'>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                 </tr>
-//                 <tr class='tableRow'>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                 </tr>
-//                 <tr class='tableRow'>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                 </tr>
-//                 <tr class='tableRow'>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                 </tr>
-//                 <tr class='tableRow'>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                 </tr>
-//                 <tr class='tableRow'>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                     <td class='cell cellDimensions'>cell</td>
-//                 </tr>
-//             </table>
-//         </div>
-//     </div>
-
-// </div>
+  // </div>
